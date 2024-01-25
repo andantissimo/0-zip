1b56c8ee08bc90432cb1b66ef82dfa74d89adb1e
MALU <contact@andantissimo.jp>
2024-01-25 22:32:05 +0900

    fix for MSVC

--- src/dostime.cc
+++ src/dostime.cc
@@ -19,6 +19,14 @@ static inline auto to_dos_time(const hours &h, const minutes &m, const seconds &
 
 tuple<uint16_t, uint16_t> to_dos_date_time(const time_point<file_clock> &mtime)
 {
+#if _MSC_VER
+    const auto t = current_zone()->to_local(utc_clock::to_sys(file_clock::to_utc(mtime)));
+    const auto d = floor<days>(t);
+    const auto y = year_month_day(d);
+    const auto h = floor<hours>(t - d);
+    const auto m = floor<minutes>(t - d - h);
+    const auto s = floor<seconds>(t - d - h - m);
+#else
     const auto t = file_clock::to_time_t(mtime);
     const auto u = duration_cast<system_clock::duration>(mtime.time_since_epoch())
                  + seconds(localtime(&t)->tm_gmtoff);
@@ -27,5 +35,6 @@ tuple<uint16_t, uint16_t> to_dos_date_time(const time_point<file_clock> &mtime)
     const auto h = floor<hours>(u - d);
     const auto m = floor<minutes>(u - d - h);
     const auto s = floor<seconds>(u - d - h - m);
+#endif
     return make_tuple(to_dos_date(y.year(), y.month(), y.day()), to_dos_time(h, m, s));
 }
